b82c7274588dd2008648ba63f1058ba2a00c1f18
Humberto M <humbertomonterrosa@gmail.com>
2023-02-21 22:03:38 -0500

    Inicio de proyecto

---
+++ src/test/java/com/trevnu/transacciones/TransaccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevnu.transacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
