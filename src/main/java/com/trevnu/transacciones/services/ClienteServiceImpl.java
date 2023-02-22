49112bcc5ba6a1a47548884b0dbbf83676a7ec9e
Humberto M <humbertomonterrosa@gmail.com>
2023-02-21 23:36:13 -0500

    Ajuste actualizar cliente

--- src/main/java/com/trevnu/transacciones/services/ClienteServiceImpl.java
+++ src/main/java/com/trevnu/transacciones/services/ClienteServiceImpl.java
@@ -7,10 +7,13 @@ import com.trevnu.transacciones.mappers.ClienteMapper;
 import com.trevnu.transacciones.repositories.ClienteRepository;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,9 +36,15 @@ public class ClienteServiceImpl implements ClienteService{
 
     @Override
     public void update(Integer id, ClienteDto dto) {
-        Cliente cliente1 = clienteRepository.findById(id).orElseThrow(ClienteNotFoundException::new);
-        BeanUtils.copyProperties(dto, cliente1);
-        clienteRepository.save(cliente1);
+        Cliente cliente = clienteRepository.findById(id).orElseThrow(ClienteNotFoundException::new);
+        BeanWrapper wrapper = new BeanWrapperImpl(dto);
+        List<String> props = Arrays.stream(wrapper.getPropertyDescriptors())
+                .filter(x -> wrapper.getPropertyValue(x.getName()) == null)
+                .map(x -> x.getName()).toList();
+
+
+        BeanUtils.copyProperties(dto, cliente, props.toArray(new String[props.size()]));
+        clienteRepository.save(cliente);
     }
 
     @Override
