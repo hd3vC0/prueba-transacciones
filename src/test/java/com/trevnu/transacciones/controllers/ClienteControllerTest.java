ab9188e5db073813fd141065f50d63a76376c881
Humberto M <humbertomonterrosa@gmail.com>
2023-03-10 07:48:43 -0500

    jacoco test report

--- src/test/java/com/trevnu/transacciones/controllers/ClienteControllerTest.java
+++ src/test/java/com/trevnu/transacciones/controllers/ClienteControllerTest.java
@@ -1,22 +1,43 @@
 package com.trevnu.transacciones.controllers;
 
+import com.trevnu.transacciones.dto.ClienteDto;
+import com.trevnu.transacciones.services.ClienteService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(ClienteController.class)
 class ClienteControllerTest {
 
-    private MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @MockBean
+    ClienteService clienteService;
 
     @Test
     void crear_cliente_retorna200() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(new ClienteController()).build();
-
+        doNothing().when(clienteService).create(ClienteDto.builder()
+                        .nombre("Juan Perez")
+                        .genero("Masculino")
+                        .edad(22)
+                        .identificacion("111111111")
+                        .direccion("calle 123")
+                        .telefono("55555555")
+                        .contrasena("segura")
+                .build());
+        when(clienteService.getAll()).thenReturn(null);
         mockMvc.perform(
                 post("/clientes")
                         .contentType("application/json")
